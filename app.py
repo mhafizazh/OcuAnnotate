778f02ec449c362c8f26968e448b38a15ef1f13d
mhafizazh <mhafiz.azh@gmail.com>
2025-05-27 09:29:37 -0600

    add requirement.txt

--- app.py
+++ app.py
@@ -4,10 +4,9 @@ from tkinter import filedialog, messagebox
 import os
 import numpy as np
 
-# Global vars
-centers = []
-radii = []
-drawing = False
+# Globals
+contours = []       # All polygons
+current_contour = []  # Points of current polygon
 temp_img = None
 original_img = None
 
@@ -18,43 +17,42 @@ def select_image_file():
                                       filetypes=[("Image files", "*.jpg *.png *.jpeg")])
 
 def mouse_callback(event, x, y, flags, param):
-    global centers, radii, drawing, temp_img
-    img = param.copy()
+    global current_contour, temp_img
 
     if event == cv2.EVENT_LBUTTONDOWN:
-        centers.append((x, y))
-        radii.append(0)
-        drawing = True
+        current_contour.append((x, y))
+        redraw(param)
 
-    elif event == cv2.EVENT_MOUSEMOVE and drawing:
-        if len(centers) > 0:
-            r = int(((x - centers[-1][0])**2 + (y - centers[-1][1])**2) ** 0.5)
-            temp_img = img.copy()
-            cv2.circle(temp_img, centers[-1], r, (0, 255, 0), 2)
-            cv2.imshow("Mark Pupils", temp_img)
+def redraw(img):
+    global temp_img
+    temp_img = img.copy()
 
-    elif event == cv2.EVENT_LBUTTONUP:
-        drawing = False
-        r = int(((x - centers[-1][0])**2 + (y - centers[-1][1])**2) ** 0.5)
-        radii[-1] = r
-        img_with_circle = img.copy()
-        for i in range(len(centers)):
-            cv2.circle(img_with_circle, centers[i], radii[i], (0, 255, 0), 2)
-        temp_img = img_with_circle
-        cv2.imshow("Mark Pupils", temp_img)
+    # Draw current in-progress polygon
+    for i in range(len(current_contour)):
+        cv2.circle(temp_img, current_contour[i], 3, (0, 255, 0), -1)
+        if i > 0:
+            cv2.line(temp_img, current_contour[i - 1], current_contour[i], (0, 255, 0), 1)
 
-def save_segmentation_mask(original_image, filename, centers, radii):
+    # Draw finalized contours
+    for poly in contours:
+        pts = np.array(poly, np.int32)
+        cv2.polylines(temp_img, [pts], isClosed=True, color=(255, 0, 0), thickness=2)
+
+    cv2.imshow("Draw Mask", temp_img)
+
+def save_segmentation_mask(original_image, filename, polygons):
     height, width = original_image.shape[:2]
     mask = np.zeros((height, width), dtype=np.uint8)
 
-    for center, radius in zip(centers, radii):
-        cv2.circle(mask, center, radius, 255, -1)  # white filled circle
+    for poly in polygons:
+        pts = np.array(poly, np.int32)
+        cv2.fillPoly(mask, [pts], 255)
 
     base_name = os.path.splitext(os.path.basename(filename))[0]
-    os.makedirs("./datasets/masked/", exist_ok=True)
+    os.makedirs("./", exist_ok=True)
     mask_filename = f"./datasets/masked/{base_name}_mask.png"
     cv2.imwrite(mask_filename, mask)
-    print(f"Saved mask: {mask_filename}")
+    print(f"Saved flexible mask: {mask_filename}")
 
 def save_blank_mask(original_image, filename):
     height, width = original_image.shape[:2]
@@ -64,10 +62,10 @@ def save_blank_mask(original_image, filename):
     os.makedirs("./datasets/masked/", exist_ok=True)
     mask_filename = f"./datasets/masked/{base_name}_mask.png"
     cv2.imwrite(mask_filename, mask)
-    print(f"No iris in frame. Saved blank mask: {mask_filename}")
+    print(f"No object in frame. Saved blank mask: {mask_filename}")
 
 def main():
-    global centers, radii, drawing, temp_img, original_img
+    global current_contour, contours, temp_img, original_img
 
     img_path = select_image_file()
     if not img_path:
@@ -82,41 +80,56 @@ def main():
     original_img = img.copy()
     temp_img = img.copy()
 
-    cv2.namedWindow("Mark Pupils")
-    cv2.setMouseCallback("Mark Pupils", mouse_callback, param=img)
+    cv2.namedWindow("Draw Mask")
+    cv2.setMouseCallback("Draw Mask", mouse_callback, param=img)
+    redraw(img)
 
     print("Instructions:")
-    print("1. Click and drag to mark center + radius of pupil/iris (1 or 2 circles).")
-    print("2. Press ENTER to save.")
-    print("3. Press N to save a blank mask (no iris).")
-    print("4. Press R to restart.")
-    print("5. Press ESC to cancel.")
+    print("- Click to draw points of a polygon.")
+    print("- Press 'C' to complete a polygon.")
+    print("- Press 'U' to undo last point.")
+    print("- Press ENTER to save the mask.")
+    print("- Press 'N' if no object (saves blank mask).")
+    print("- Press 'R' to restart drawing.")
+    print("- Press ESC to cancel.")
 
     while True:
-        cv2.imshow("Mark Pupils", temp_img)
+        cv2.imshow("Draw Mask", temp_img)
         key = cv2.waitKey(1) & 0xFF
 
         if key == 13:  # ENTER
-            if 1 <= len(centers) <= 2 and len(centers) == len(radii):
-                save_segmentation_mask(original_img, img_path, centers, radii)
+            if contours:
+                save_segmentation_mask(original_img, img_path, contours)
                 break
             else:
-                messagebox.showerror("Error", "Please draw 1 or 2 circles before saving.")
+                messagebox.showerror("Error", "No mask drawn. Press 'N' to save blank mask.")
+
+        elif key == ord('c'):
+            if len(current_contour) >= 3:
+                contours.append(current_contour.copy())
+                current_contour.clear()
+                redraw(original_img)
+            else:
+                print("Need at least 3 points to form a polygon.")
+
+        elif key == ord('u'):
+            if current_contour:
+                current_contour.pop()
+                redraw(original_img)
+
+        elif key == ord('n'):
+            print("Marked as 'No object in frame'")
+            save_blank_mask(original_img, img_path)
+            break
+
+        elif key == ord('r'):
+            print("Restarting drawing...")
+            current_contour = []
+            contours = []
+            redraw(original_img)
 
         elif key == 27:  # ESC
             print("Canceled.")
-            break
-
-        elif key == ord('r'):  # Restart
-            print("Restarting annotation...")
-            centers = []
-            radii = []
-            temp_img = original_img.copy()
-            cv2.imshow("Mark Pupils", temp_img)
-
-        elif key == ord('n'):  # No iris
-            print("Marked as 'No Iris in Frame'")
-            save_blank_mask(original_img, img_path)
             break
 
     cv2.destroyAllWindows()
